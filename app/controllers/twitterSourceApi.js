010d5ce4f1819ffe41153596a25b163fecbfd17e
Simen Bekkhus <sbekkhus91@gmail.com>
2013-11-23 17:19:18 +0100

    Make a call to the twitter-api, returning tweets

--- app/controllers/twitterSourceApi.js
+++ app/controllers/twitterSourceApi.js
@@ -1,10 +1,22 @@
-var mongoose = require('mongoose'),
-    MediaPlayer = mongoose.model('MediaPlayer');
+var oauth = require('oauth');
 
-// *********** TWITTER API ************
-// GET - read all
 exports.readAll = function () {
-    return function (req, res) {
-        res.json('{"test: "bah"}');
-    };
+    return function (rew, res) {
+        var callback = function (error, data, res) {
+            // TODO: Parse/filter JSON
+            console.log(JSON.stringify(data));
+        };
+
+
+
+        oa.get('https://api.twitter.com/1.1/search/tweets.json?q=%23ACNTech', token, secrettoken, function (error, data, response) {
+            if (error) {
+                callback(error, response, 'https://api.twitter.com/1.1/search/tweets.json?q=#ACNTech');
+            } else {
+                callback(null, JSON.parse(data), response);
+            }
+        });
+
+        res.json('{"hei": 5}');
+    }
 };
